0a2c662d1cf9c21a7e1f7ffea86430eae94fa463
xcrn <xcrnvieira@gmail.com>
2021-09-15 22:54:20 -0300

    initial commit

---
+++ src/views/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Calcular quadrado</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Calculadora de Quadrado</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+        <form @submit.prevent="calculo">
+          <div>
+              <label>Calcular o quadrado do número: </label>
+          </div>
+          <input v-model.number="num1" />
+
+           <ion-button v-on:click="calculo" type="submit">Calcular</ion-button>
+        </form>
+        <div  class="result">
+          <p><strong>Resultado: {{ resultadoFinal }} </strong></p>
+        </div>
+      </div>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      num1: 0,
+      resultadoFinal: 0, 
+      numero: 0,
+    };
+  },
+  methods: {
+   calculo() {
+    const { resultadoFinal, num1 } = this;
+    this.resultadoFinal = (num1 * num1);
+    console.log(resultadoFinal);
+    }
+  },
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+};
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+
+}
+
+form {
+  border: 1px solid #8c8c8c;
+  border-radius: 3px;
+  margin: 0 35% 1%;
+  padding: 25px;
+}
+
+input {
+  margin: 5px;
+  width: 80px;
+  height: 40px;
+  border: 0px;
+  border-bottom: 1px solid #8c8c8c;
+}
+
+button {
+  background-color: blue;
+  color: aliceblue;
+  width: 100px;
+  font-size: 14px;
+  padding: 5px 0;
+  border-radius: 3px;
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
